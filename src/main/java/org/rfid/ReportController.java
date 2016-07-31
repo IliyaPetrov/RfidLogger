d93e2deb7d267c28bd1c90b15b8793e978947d8b
IliyaPetrov <egoist82@gmail.com>
2016-07-31 17:03:33 +0300

    Date implemented.

--- src/main/java/org/rfid/ReportController.java
+++ src/main/java/org/rfid/ReportController.java
@@ -14,10 +14,10 @@ public class ReportController {
     @Autowired
     EventRepository repository;
 
-    @RequestMapping(value = "/report/{rfid}", method = RequestMethod.GET)
+    @RequestMapping(value = "/report/{rfid}/{date}", method = RequestMethod.GET)
     @ResponseBody
-    public Report reportControllerGet(@PathVariable String rfid) {
-        List<Event> minMax = repository.findByRfidAndDateBetweenOrderByDate(rfid,1L,2L);
+    public Report reportControllerGet(@PathVariable String rfid,@PathVariable long date) {
+        List<Event> minMax = repository.findByRfidAndDateBetweenOrderByDate(rfid,date,date+(24*60*60));
         if (minMax.isEmpty()){
             return new Report(0L, 0L, rfid);
         } else if (minMax.size()==1){
